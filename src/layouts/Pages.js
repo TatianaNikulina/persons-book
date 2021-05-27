1adf06185021a071cafd58c7d10a2067724e6eba
Ignaty Kashnitsky <noir.ik@gmail.com>
2021-05-27 20:17:33 +0200

    Add Profile and Editing mode

--- src/layouts/Pages.js
+++ src/layouts/Pages.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom'
 import AddNewPerson from "../components/Persons/AddNewPerson";
 import Home from "../components/Home/Home";
 import Persons from "../components/Persons/Persons";
+import PersonProfile from "../components/Persons/PersonProfile";
 
 const Pages = () => {
     return(
@@ -15,6 +16,9 @@ const Pages = () => {
             </Route>
             <Route path="/persons/add">
                 <AddNewPerson />
+            </Route>
+            <Route path="/persons/:id">
+                <PersonProfile />
             </Route>
         </Switch>
     )
